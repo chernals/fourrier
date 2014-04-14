f1c1410c3ef2d0fe1152454dffb9096cea3a9714
Cedric Hernalsteens <cedric@cedrich.net>
2014-04-14 10:38:37 +0200

    Started to work on the Ruby interface

--- examples/ruby/fourrier.rb
+++ examples/ruby/fourrier.rb
@@ -21,8 +21,49 @@ module Fourrier
   extend FFI::Library
   ffi_lib "../../lib/fourrier.so"
   attach_function :Hanning_r, [ :pointer, :uint, :pointer], :void
+  
+  # Freqs
+  attach_function :allocFrequencies, [ :uint ], :pointer
+  attach_function :getHarmonics, [ :pointer ], :uint
+  attach_function :getFrequency, [ :pointer, :uint ], :double
+  attach_function :getAmplitude, [ :pointer, :uint ], :double
+  attach_function :setAmplitude, [ :pointer, :double, :uint ], :void
+  attach_function :getPhase, [ :pointer, :uint ], :double
+  attach_function :setPhase, [ :pointer, :double, :uint], :void
+    
+  class Freqs
+    def initialize(harmonics)
+      @p = Fourrier.allocFrequencies(harmonics)
+    end
+    
+    def uninitialize
+      
+    end
+    
+    def getHarmonics
+      Fourrier.getHarmonics(@p)
+    end
+    
+    def getAmplitude(h)
+      Fourrier.getAmplitude(@p, h)
+    end
+    
+    def setAmplitude(value, h)
+      Fourrier.setAmplitude(@p, value, h)
+    end
+    
+    def getPhase(h)
+      Fourrier.getPhase(@p, h)
+    end
+    
+    def setPhase(value, h)
+      Fourrier.setPhase(@p, value, h)
+    end
+    
+  end # class Freqs
 
 end
+
 
 signal = []
 File.open("signal.dat", 'r').each_line {|line| signal << line.to_f}
@@ -33,5 +74,6 @@ _windowed = LibC.malloc(8 * signal.size)
 Fourrier.Hanning_r(_signal, signal.size, _windowed)
 windowed = _windowed.get_array_of_float64(0,signal.size)
 
-
-#Hello.puts("Hello, World")
+f = Fourrier::Freqs.new(10)
+f.setAmplitude(1.1,2)
+puts f.getAmplitude(2)
